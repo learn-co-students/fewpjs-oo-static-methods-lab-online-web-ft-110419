918dc9acf959e0c15b626fd736b82f6e0b013812
jessesbyers <jesse.s.byers@gmail.com>
2020-03-02 13:54:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-' ']+/g, '');  
+  }
+
+  static titleize(string) {
+    let array = string.toLowerCase().split(" ")
+    let newString = []
+
+    for(let i = 0; i< array.length; i++){
+      if (i === 0) {
+        newString.push(array[i][0].toUpperCase() + array[i].slice(1))
+      } else if (array[i] === 'the' || array[i] === 'a' || array[i] === 'an' || array[i] === 'but' || array[i] === 'of' || array[i] === 'and' || array[i] === 'for' || array[i] === 'at' || array[i] === 'by'  || array[i] === 'from') {
+        newString.push(array[i])  
+      } else {
+        newString.push(array[i][0].toUpperCase() + array[i].slice(1))
+      }
+    }
+    return newString.join(" ")
+  }
 }

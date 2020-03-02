6728774299493e2904f5ac31a0cd705657533bef
Brandon Weaver <a7xncob@gmail.com>
2020-03-02 03:50:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+  }
+  
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '');
+  }
+  
+  static titleize(string) {
+    const titleizedStringArray = [];
+    const stringArray = string.split(' ');
+    const preservedWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    titleizedStringArray.push(this.capitalize(stringArray[0]));
+    for (let i = 1; i < stringArray.length; i++) {
+      if (preservedWords.includes(stringArray[i])) {
+        titleizedStringArray.push(stringArray[i]);
+      }
+      else {
+        titleizedStringArray.push(this.capitalize(stringArray[i]));
+      }
+    }
+    return titleizedStringArray.join(' ');
+  }
 }

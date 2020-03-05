cfd54a7e0c74ad85f7f7c2ba77c92b7aae67f2b9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-05 17:00:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return `${str[0].toUpperCase()}${str.substring(1)}`;
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^\w\d\s-']/gi, '');
+  }
+
+  static titleize(str) {
+    const exemptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    return str.split(" ").map((word, index) => {
+      debugger
+      return index === 0 || !exemptWords.find(exemptWord => exemptWord === word) ?
+        Formatter.capitalize(word) :
+        word;
+    }).join(" ");
+  }
 }

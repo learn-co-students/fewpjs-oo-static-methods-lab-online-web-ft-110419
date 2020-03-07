d24f879b2b4a440ecc7b55a22e02f5e82fd18659
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-07 13:25:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  constructor(){}
+  static capitalize(string){
+    return string.replace(string[0], string[0].toUpperCase());
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9_\s'-]/g, "");
+  }
+  static titleize(string){
+    let noWord = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    let newString = string.split(" ").map((word, i) => {
+      return !noWord.includes(word) || i === 0 ? this.capitalize(word) : word;
+     
+      })
+       return newString.join(" ")
+    }
 }

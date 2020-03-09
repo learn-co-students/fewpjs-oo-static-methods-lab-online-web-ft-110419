17be23085cfb8b2d2e79a734b638da6f731ce9f8
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-09 15:45:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    let words = string.split(' ')
+    const sanitized = words.map( word => word.replace(/[^A-Za-z-']+/g, ''))
+    return sanitized.join(' ')
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'an', 'a', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = string.split(' ')
+    const lead = words[0].charAt(0).toUpperCase() + words[0].slice(1)
+    const rest = words.slice(1)
+    const upRest = []
+
+    for (let word of rest) {
+      if (!exceptions.includes(word)) {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      upRest.push(word)
+    }
+    const title = (lead + ' ' + upRest.join(' '))
+    return title
+  }
 }
